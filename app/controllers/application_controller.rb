f2252fd7ea62bfed19456b402a0ff7a94cbd3916
Elizabeth Klenczar <eklenczar@gmail.com>
2023-09-26 02:45:02 -0400

    Add validations to application controller and controller actions to kittens_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+
+
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    private
+
+    def render_unprocessable_entity(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def render_not_found_response(error)
+        render json: {message: error.message}, status: :not_found
+    end
 end
--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,44 @@
 class KittensController < ApplicationController
+
+    # GET /kittens
+    def index
+        kittens = Kitten.all
+        render json: kittens
+    end
+
+    # GET /kittens/:id
+    def show
+        kitten = find_kitten
+        render json: kitten
+    end
+
+    # POST /kittens
+    def create
+        kitten = Kitten.create!(kitten_params)
+        render json: kitten, status: :created
+    end
+
+    # PATCH /kittens/:id 
+    def update
+        kitten = find_kitten
+        kitten.update(kitten_params)
+        render json: kitten, status: :ok
+    end
+
+    # DELETE /kittens/:id
+    def destroy
+        kitten = find_kitten
+        kitten.destroy
+        render json: kitten
+    end
+
+    private
+
+    def kitten_params
+        params.permit(:name, :breed, :color, :gender, :image)
+    end
+
+    def find_kitten
+        Kitten.find(params[:id])
+    end
 end
